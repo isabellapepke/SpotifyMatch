2ae2ee8b5e463b8ffa8270118076ee349ab26b24
Isabella Pepke <pepke101@mail.chapman.edu>
2018-11-13 23:45:18 -0800

    update with daniel's edits
    cleaner code, converted code into functions, prints all saved tracks

--- spotifyxx.py
+++ spotifyxx.py
@@ -1,31 +1,58 @@
+### Created by Daniel Brisen, Nic Fergie, and Bella Pepke
+
 import os
 import sys
 import json
 import spotipy
 import webbrowser
 import spotipy.util as util
-#from json.decoder import JSONDecodeError
 
-#Get the username from terminal, use argv taking the argument from commandline
+
+def auth(user):
+    "Function obtains authorization from spotify, returns authorization token"
+
+    try:
+        #Set Auth variables
+        SPOTIFY_CLIENT_ID = '4814686cacd44e65ab9561e8744726ee'
+        SPOTIPY_CLIENT_SECRET = 'e23f210b1e0d4e6da5dbe263a7add7af'
+        SPOTIPY_REDIRECT_URI = 'http://localhost:/callback'
+        scope = 'user-read-private user-read-email user-library-read'
+        #Get Auth token
+        token = util.prompt_for_user_token(user,scope ,SPOTIFY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI)
+        return token
+    except:
+        #if auth fails ... may be due to out-dated cache
+        print("Could not obtain token for , " + user)
+        print("Clearing Cache, please run again")
+        #clear cache, exit gracefully
+        #os.remove(f".cache-{user}")
+        sys.exit(0)
+
+def getTracks(token):
+    "Function returns list containing song library"
+
+    sp = spotipy.Spotify(auth=token)#create Spotify object
+    songs = []
+    offset = 0 #this variable determines index in song library at which to begin song fetch
+
+    #begin fetching songs from library
+    results = sp.current_user_saved_tracks(20,offset)
+    while( not (results['items'] == []) ): #while there are still songs left in library
+        for item in results['items']:
+            track= item['track']
+            songs.append(track)
+
+        #increase offset to fetch next batch of songs, then fetch
+        offset+=20
+        results = sp.current_user_saved_tracks(20,offset)
+    return songs
+
+def printTracks(songs):
+    for song in songs:
+        print(song['name']+ ' - ' + song['artists'][0]['name']).encode("utf-8")
+
+
 username = sys.argv[1]
-
-# brownbearhoopla?si=GTQRYsmTTHivygieza7QbA
-
-#erase cache and prompt for user permission, ask user to accept
-
-#token  = util.prompt_for_user_token(username)
-clientID = '112fe974da5743d4a85569ae8a4804da'
-clientSecret = 'd82f12a424fa493092654ad56cfad2c5'
-redirectURL = 'http://localhost:8888/callback/'
-token = util.prompt_for_user_token(username,scope='user-read-private user-read-email user-library-read',client_id='112fe974da5743d4a85569ae8a4804da',client_secret='d82f12a424fa493092654ad56cfad2c5',redirect_uri='http://www.google.com/') # We/Spotify suggest http://localhost:8888/callback/ or http://localhost/
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name']).encode("utf-8")
-else:
-    print "Can't get token for", username
-
-#create our spotifyObject
-spotifyObject = spotipy.Spotify(auth=token)
+token = auth(username)
+songs= getTracks(token)
+printTracks(songs)
