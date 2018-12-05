3ef38698217ff931bf51d8762932003e81be7f29
brisenodaniel <brise107@mail.chapman.edu>
2018-12-04 23:46:31 -0800

    Adds Comparer class

---
+++ comparer.py
@@ -0,0 +1,48 @@
+from playlist import Playlist 
+from statistics import Statistics
+
+class Comparer:
+    """Should call setSimilarity() immediately after creating Comparer object"""
+
+    def __init__(self, playlist1, playlist2):
+        self.playlist1 = playlist1
+        self.playlist2 = playlist2
+        self.comparison = playlist1.compare(playlist2)
+        self.calculator = Statistics()
+        self.similarity = self.setSimilarity()
+    
+
+    def printCommonSongs(self):
+        print(self.comparison[0])
+    
+    def setSimilarity(self):
+        plen1 = len(self.playlist1.songs)
+        plen2 = len(self.playlist2.songs)
+        sharedlen = len(self.comparison[1])
+        artistslen = len(self.comparison[2])
+        similarity = self.calculator.similarityMetric(plen1, plen2, sharedlen, artistslen)
+        return similarity
+        
+    def isSimilar(self, minSimilarity):
+        return self.similarity >= minSimilarity
+
+    def getMissingSongs(self, profile = 0):
+        if profile == 0 :
+            missingSongs = Playlist("missing from User 1", [])
+            for song in self.playlist2.songs:
+                if song in self.playlist1.songs:
+                    continue
+                missingSongs.add(song)
+            return missingSongs
+        
+        missingSongs = Playlist("missing from User 2", [])
+        for song in self.playlist1.songs:
+            if song in self.playlist2.songs:
+                continue 
+            missingSongs.add(song)
+        return missingSongs
+
+
+
+            
+    
