8f4f7961598c0f8e85cd13be5341c1cfedcb7df7
brisenodaniel <brise107@mail.chapman.edu>
2018-12-04 22:34:21 -0800

    Adds implementation of Song and Playlist classes

--- README_BRISENO.md
+++ README_BRISENO.md
@@ -3,6 +3,7 @@
     * Edited code so that all songs in a user's library are downloaded, not just first 20
     * Re-factored code to include good use of functions and to make the code more readable
     * Created comparison algorithm
+    * Wrote and implemented Song, Playlist, and Statistics classes
 **Currently Working On**
 
     * Creating Song and Playlist classes to more easily compare two profiles for similarity 
--- spotifyxx.py
+++ spotifyxx.py
@@ -20,9 +20,17 @@ from playlist import Playlist
 
 
 def wait(time):
-    "Function delays program by a deteremined amount of seconds"
+    "Function delays program by a deteremined amount of seconds--used for waiting for logout page"
+
     sleep(time)
 
+def deleteCache():
+    "Function deletes cache, raises exception if does not execute"
+
+    try:
+        os.remove(f,".cache-{user}")
+    except:
+        print("There was an error when trying to delete cache. \nYou must manually delete it after everytime you run the code.")
 
 def auth(user):
     "Function obtains authorization from spotify, returns authorization token"
@@ -35,31 +43,33 @@ def auth(user):
         scope = 'user-read-private user-read-email user-library-read'
         #Get Auth token
         token = util.prompt_for_user_token(user,scope ,SPOTIFY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI)
+        print("\nAuthorized!\n")
         return token
     except:
         #if auth fails ... may be due to out-dated cache
         print("Could not obtain token for , " + user)
         print("Clearing Cache, please run again")
-        #clear cache, exit gracefully, bellas environment will not run this line, comment if error
-        os.remove(f".cache-{user}")
-        #os.remove(f,".cache-{user}")
+        deleteCache()
         sys.exit(0)
 
-def getTracks(token, username):
+def makePlaylist(token, username):
     "Function returns list containing song library"
-    sp = spotipy.Spotify(auth=token)#create Spotify object
+
+    sp = spotipy.Spotify(auth=token) #create Spotify object
     songs = []
-    playlist = Playlist(username, [])
     offset = 0 #this variable determines index in song library at which to begin song fetch
+    playlist = Playlist(username, [])
     #begin fetching songs from library
     results = sp.current_user_saved_tracks(20,offset)
     while( not (results['items'] == []) ): #while there are still songs left in library
         for item in results['items']:
             track= item['track']
             songs.append(track)
+
         #increase offset to fetch next batch of songs, then fetch
         offset+=20
         results = sp.current_user_saved_tracks(20,offset)
+    #create playlist object from json string of songs
     for song in songs:
         artists = []
         for artist in song['artists']:
@@ -68,56 +78,59 @@ def getTracks(token, username):
         playlist.add(track)
     return playlist
 
-def printTracks(playlist):
-    "Function prints the tracks in a playlist"
-    print(str(playlist))
-        #if you get a utf error insert .encode("utf-8") like so:
-        #print(song['name']+ ' - ' + song['artists'][0]['name']).encode("utf-8")
-
-def get_playlist_tracks(username,playlist_id):
-    results = sp.user_playlist_tracks(username,playlist_id)
-    tracks = results['items']
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
-    return tracks
-
-
 def printCommonTracks(playlist1, playlist2):
+    "Function prints tracks that are in common between two users"
     commonSongs = playlist1.compare(playlist2)[0]
     print("\n\n\n\nNow printing common songs...\n\n*************--------------------*************")
-    print(str(commonSongs))
+    try:
+        print(commonSongs)
+    except:
+        print(str(commonSongs).encode())
 
 def logoutUser():
     "Function logs out a user"
-    print("Now logging you out...")
+
+    print("\n\nNow logging you out...\n\n")
     wait(5)
     webbrowser.open("http://www.spotify.com/logout")
     wait(10)
+
+
+###########----Main----############
+
+#Check for valid amount of command line arugments
+if len(sys.argv) < 2:
+    print("You must provide 2 Spotify Usernames as command-line arguments")
+    print("Now exiting program")
+    sys.exit()
+
 
 #when program starts logout any users, allow time for page to load
 webbrowser.open("http://www.spotify.com/logout")
 wait(5)
 
+songs1 = []
+songs2 = []
+username1 = ""
+username2 = ""
 
-username1 =sys.argv[1]
+username1 = sys.argv[1]
 token1 = auth(username1)
-songs1 = getTracks(token1, username1)
-#must run this code to remove cache for bellas computer:
-#wait(5)
-#os.remove("C:\Users\isabe\Documents\Networks\SpotifyMatch\.cache-brownbearhoopla")
+songs1 = makePlaylist(token1, username1)
+print("########### Songs in Playlist 1 ##############")
+print(songs1)
+wait(5)
 logoutUser()
 
 
 username2 = sys.argv[2]
 token2 = auth(username2)
-songs2 = getTracks(token2, username2)
-#must run this code to remove cache for bellas computer:
-#wait(5)
-#os.remove("C:\Users\isabe\Documents\Networks\SpotifyMatch\.cache-wvvk3mob8c4qncj1wb6jxmv6m")
+songs2 = makePlaylist(token2, username2)
+print("########### Songs in Playlist 2 ##############")
+print(songs2)
+wait(5)
 logoutUser()
 
 
 printCommonTracks(songs1, songs2)
 print("******************************** --------------------  ********************************")
-print
