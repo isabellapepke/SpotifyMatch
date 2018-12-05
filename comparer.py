eaa991479eae981d8a6ce64fc5608e64f59a617c
brisenodaniel <brise107@mail.chapman.edu>
2018-12-05 13:41:53 -0800

    Adds Flake8 compliance for comparer class

--- comparer.py
+++ comparer.py
@@ -1,19 +1,19 @@
-from playlist import Playlist 
+from playlist import Playlist
 from statistics import Statistics
 
-class Comparer:
 
+class Comparer:
+    
     def __init__(self, playlist1, playlist2):
         self.playlist1 = playlist1
         self.playlist2 = playlist2
         self.comparison = playlist1.compare(playlist2)
         self.calculator = Statistics()
         self.similarity = self.setSimilarity()
-    
 
     def printCommonSongs(self):
         print(self.comparison[0])
-    
+
     def setSimilarity(self):
         plen1 = len(self.playlist1.songs)
         plen2 = len(self.playlist2.songs)
@@ -21,7 +21,7 @@ class Comparer:
         artistslen = self.comparison[2]
         similarity = self.calculator.similarityMetric(plen1, plen2, sharedlen, artistslen)
         return similarity
-        
+
     def isSimilar(self, minSimilarity):
         return self.similarity >= minSimilarity
 
@@ -37,11 +37,6 @@ class Comparer:
         missingSongs = Playlist("missing from User 2", [])
         for song in self.playlist1.songs:
             if song in self.playlist2.songs:
-                continue 
+                continue
             missingSongs.add(song)
-        return missingSongs
-
-
-
-            
-    
+        return missingSongs  
